45334ac3204cb1955782c2817bee4a0edc4da193
bbarres <benoit.barres@gmail.com>
2023-06-21 16:01:38 +0200

    correction pour Caroline

--- Myzus_spiro_reg.R
+++ Myzus_spiro_reg.R
@@ -18,17 +18,17 @@ dataReg<-read.table("data/ExtractionR_MyzusSpiro_72H.txt",
 ##############################################################################/
 
 #aggregating the number of individuals per rep and dose
-dataRegRep<-as.data.frame(aggregate(cbind(nb_vi,nb_mtot)~dose+rep_test,
+dataRegRep<-as.data.frame(aggregate(cbind(nb_vi,nb_mtot)~dose+dat_test,
                     data=dataReg,"sum"))
 #the maximum number of individual for one repetition and one dose
 ymasc<-max((dataRegRep$nb_vi+dataRegRep$nb_mtot))
 #plotting the distribution of dead / alive for each repetition
-op<-par(mfrow=c(3,4))
-for (i in c(1:length(levels(dataRegRep$rep_test)))) {
-  temp<-dataRegRep[dataRegRep$rep_test==levels(dataRegRep$rep_test)[i],]
+op<-par(mfrow=c(2,2))
+for (i in c(1:length(levels(as.factor(dataRegRep$dat_test))))) {
+  temp<-dataRegRep[dataRegRep$dat_test==levels(as.factor(dataRegRep$dat_test))[i],]
   barplot(t(as.matrix(temp[,c(3:4)])),names.arg=temp$dose,
           las=1,xlab="Dose",ylab="Number",ylim=c(0,ymasc),
-          main=levels(dataRegRep$rep_test)[i])
+          main=levels(dataRegRep$dat_test)[i])
 }
 par(op)
 
@@ -49,16 +49,18 @@ barplot(t(as.matrix(dataRegDos[,c(2:3)])),
 temp.m1<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
              data=dataReg,
-             curveid=rep_test,
+             curveid=dat_test,
              fct=LN.3u(),type="binomial")
 plot(temp.m1,ylim=c(0,1.1),xlim=c(0,100),
      main=names(table(dataReg$ech_id))[1],
-     ylab="mortality rate")
+     ylab="mortality rate",legendPos=c(10,0.2))
 compParm(temp.m1,"e")
+EDcomp(temp.m1,c(50,50))
+ED50m1<-ED(temp.m1,0.5,type="absolute")
 
 temp.m2<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
              weights=(nb_mtot+nb_vi),
-             data=dataReg,
+             data=dataRegRep,
              fct=LN.3u(),type="binomial")
 ED50v<-ED(temp.m2,0.5,type="absolute")
 plot(temp.m2,ylim=c(0,1.1),xlim=c(0,100),
