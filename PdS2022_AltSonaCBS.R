ac92535ccbb840b5c260a863d339078b224c33a8
bbarres <benoit.barres@gmail.com>
2023-08-18 10:57:53 +0200

    additional analyses of regression

---
+++ PdS2022_AltSonaCBS.R
@@ -0,0 +1,92 @@
+##############################################################################/
+##############################################################################/
+#Analysis of Alternaria solani CBS
+##############################################################################/
+##############################################################################/
+
+#loading the packages necessary for the analysis
+library(drc)
+library(plotrix)
+library(gdata)
+
+AltSolCBS<-read.table("data/2022_alternaria_solani_CBS.txt",
+                      header=TRUE,stringsAsFactors=TRUE,sep=";")
+AltSolCBS$pest_sa_id<-paste(AltSolCBS$pest_sa_id,AltSolCBS$synerg_id,sep="-")
+AltSolCBS$pest_sa_id<-as.factor(AltSolCBS$pest_sa_id)
+
+
+##############################################################################/
+#Regression analysis of population germination for 2021 monitoring plan####
+##############################################################################/
+
+datamyc<-AltSolCBS[AltSolCBS$lect_echec!=1,]
+
+#first we extract the list of the different SA listed in the file
+SAlist<-levels(datamyc$pest_sa_id)
+#creating the empty result output file
+CompRez<-data.frame(Species=character(),Subs_Act=factor(),
+                    sample_ID=factor(),read_time=factor(),
+                    ED50=character(),ED95=character(),ED99=character())
+
+#we make a subselection of the data according to the SA
+pdf(file="output/plot_AltSolaCBS.pdf",width=7)
+for (j in 1:length(SAlist)) {
+  data_subSA<-datamyc[datamyc$pest_sa_id==SAlist[j],]
+  data_subSA$ech_id<-drop.levels(data_subSA$ech_id)
+  #some individual never reach an inhibition of 50%, event for the highest 
+  #tested concentration. 
+  SA_rez<-as.character(data_subSA[data_subSA$dose==max(data_subSA$dose) 
+                                  & data_subSA$rslt_03>50,
+                                  "ech_id"])
+  Esp_rez<-as.character(data_subSA[data_subSA$dose==max(data_subSA$dose) 
+                                   & data_subSA$rslt_03>50,
+                                   "bioagr_id"])
+  ifelse(length(SA_rez)==0,
+         REZSA<-data.frame(Species=character(),Subs_Act=factor(),
+                           sample_ID=factor(),
+                           read_time=factor(),ED50=character(),
+                           ED95=character(),ED99=character()),
+         REZSA<-data.frame("Species"=Esp_rez,
+                           "Subs_Act"=SAlist[j],"sample_ID"=SA_rez,
+                           "read_time"=data_subSA$tps_expo[1],
+                           "ED50"=paste(">",max(data_subSA$dose),sep=""),
+                           "ED95"=paste(">",max(data_subSA$dose),sep=""),
+                           "ED99"=paste(">",max(data_subSA$dose),sep=""))
+  )
+  #we limit the dataset to the sample that reach somehow a IC of 50%
+  if(dim(data_subSA[!(data_subSA$ech_id %in% SA_rez),])[1]!=0) {
+    SA.dat<-data_subSA[!(data_subSA$ech_id %in% SA_rez),]
+    SA.dat<-drop.levels(SA.dat)
+    for (i in 1:dim(table(SA.dat$ech_id))[1]) {
+      tempdat<-SA.dat[SA.dat$ech_id==names(table(SA.dat$ech_id))[i],]
+      temp.m1<-drm(rslt_03~dose,
+                   data=tempdat,
+                   fct=LL.4())
+      plot(temp.m1,ylim=c(0,110),xlim=c(0,50),pch=19,
+           main=paste(tempdat$bioagr_id[1],
+                      SAlist[j],names(table(SA.dat$ech_id))[i]))
+      plot(temp.m1,type="obs",add=TRUE,col="red")
+      temp<-ED(temp.m1,c(50,5,1),type="absolute")
+      tempx<-data.frame("Species"=tempdat$bioagr_id[1],
+                        "Subs_Act"=SAlist[j],
+                        "sample_ID"=names(table(SA.dat$ech_id))[i],
+                        "read_time"=data_subSA$tps_expo[1],
+                        "ED50"=as.character(temp[1]),
+                        "ED95"=as.character(temp[2]),
+                        "ED99"=as.character(temp[3]))
+      REZSA<-rbind(REZSA,tempx)}} else {
+        REZSA<-REZSA
+      }
+  CompRez<-rbind(CompRez,REZSA)
+}
+dev.off()
+
+#exporting the result as a text file
+CompRez<-CompRez[order(CompRez$Subs_Act,CompRez$sample_ID),]
+write.table(CompRez, file="output/results_AltSolaCBS.txt",
+            sep="\t",quote=FALSE,row.names=FALSE)
+
+
+##############################################################################/
+#END
+##############################################################################/
---
+++ PlasmoCyazo_Amis.R
@@ -0,0 +1,87 @@
+##############################################################################/
+##############################################################################/
+#Script for Plasmopara viticola CI50 cyazo vs amisulbrom
+##############################################################################/
+##############################################################################/
+
+#loading the packages necessary for the analysis
+library(drc)
+library(plotrix)
+library(gdata)
+library(tidyr)
+library(RColorBrewer)
+
+#loading the data
+datamyc<-read.table("data/2020-22_mildiouVigne_CDR_AmiCyaz.txt",
+                     header=TRUE,stringsAsFactors=TRUE,sep=";")
+
+
+##############################################################################/
+#Regression analysis of mycelial growth experiment 14 days####
+##############################################################################/
+
+datamyc<-datamyc[datamyc$lect_echec!=1,]
+datamyc<-drop.levels(datamyc)
+
+#first we extract the list of the different SA listed in the file
+SAlist<-levels(datamyc$pest_sa_id)
+CompRez<-data.frame(Subs_Act=factor(),sample_ID=factor(),read_time=factor(),
+                    ED50=character(),ED95=character(),ED99=character())
+
+#we make a subselection of the data according to the SA
+pdf(file="output/plot_cyazo_vs_amisul.pdf",width=7)
+for (j in 1:length(SAlist)) {
+  data_subSA<-datamyc[datamyc$pest_sa_id==SAlist[j],]
+  data_subSA$ech_id<-drop.levels(data_subSA$ech_id)
+  
+  REZSA<-data.frame(Subs_Act=factor(),sample_ID=factor(),
+                    read_time=factor(),ED50=character(),
+                    ED95=character(),ED99=character())
+  
+  for (i in 1:dim(table(data_subSA$ech_id))[1]) {
+    tempdat<-data_subSA[data_subSA$ech_id==names(table(data_subSA$ech_id))[i],]
+    if(tempdat[tempdat$dose==max(tempdat$dose),"rslt_03"]>50) {
+      tempx<-data.frame("Subs_Act"=SAlist[j],"sample_ID"=tempdat$ech_id[1],
+                        "read_time"=data_subSA$tps_expo[1],
+                        "ED50"=paste(">",max(tempdat$dose),sep=""),
+                        "ED95"=paste(">",max(tempdat$dose),sep=""),
+                        "ED99"=paste(">",max(tempdat$dose),sep=""))
+    } else {
+      temp.m1<-drm(rslt_03~dose,
+                   data=tempdat,
+                   fct=LL.3())
+      plot(temp.m1,ylim=c(0,110),xlim=c(0,100),
+           main=paste(SAlist[j],as.character(tempdat$ech_id[1])))
+      temp<-ED(temp.m1,c(50,5,1),type="absolute")
+      tempx<-data.frame("Subs_Act"=SAlist[j],
+                        "sample_ID"=as.character(tempdat$ech_id[1]),
+                        "read_time"=data_subSA$tps_expo[1],
+                        "ED50"=as.character(temp[1]),
+                        "ED95"=as.character(temp[2]),
+                        "ED99"=as.character(temp[3]))
+    }
+    
+    REZSA<-rbind(REZSA,tempx)
+  }
+  CompRez<-rbind(CompRez,REZSA)
+}
+dev.off()
+
+write.table(CompRez, file="output/results_cyazo_vs_amisul.txt",
+            sep="\t",quote=FALSE,row.names=FALSE)
+
+
+##############################################################################/
+#Looking at the correlation between amisulbrom and cyazofamid CI50####
+##############################################################################/
+
+plot(log(as.numeric(CompRez$ED50[1:4])),log(as.numeric(CompRez$ED50[5:8])),
+     xlab="CI50 amisulbrom SHAM",ylab="CI50 cyazofamid SHAM")
+plot(as.numeric(CompRez$ED50[1:4]),as.numeric(CompRez$ED50[5:8]),
+     xlab="CI50 amisulbrom SHAM",ylab="CI50 cyazofamid SHAM",
+     xlog=TRUE,ylog=TRUE,log="xy")
+
+
+##############################################################################/
+#END
+##############################################################################/
